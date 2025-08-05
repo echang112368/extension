a87a4dd04f2040d10e98ea8bf4e6fd016e22c263
echang112368 <echang368@gmail.com>
2025-08-05 14:02:28 -0700

    updates

---
+++ content.js
@@ -0,0 +1,14 @@
+(function() {
+  const isShopifyCheckout = () => {
+    const { hostname, pathname } = window.location;
+    const hostMatch =
+      hostname === 'checkout.shopify.com' || hostname.endsWith('.myshopify.com');
+    const pathMatch = /\/checkouts?/i.test(pathname);
+    const checkoutObj = typeof window.Shopify !== 'undefined' && Shopify.Checkout;
+    return (hostMatch && pathMatch) || checkoutObj;
+  };
+
+  if (isShopifyCheckout()) {
+    chrome.runtime.sendMessage({ action: 'openPopup' });
+  }
+})(); 

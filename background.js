1eb7bcda89a826455fb7d64e6bceeb8adbd04d37
echang112368 <echang368@gmail.com>
2025-08-10 12:48:10 -0700

    Revert "Auto-open popup on Shopify checkout"

--- background.js
+++ background.js
@@ -4,35 +4,4 @@ chrome.runtime.onMessage.addListener((message) => {
   }
 });
 
-const isShopifyCheckout = (url) => {
-  try {
-    const { hostname, pathname } = new URL(url);
-    if (hostname === 'checkout.shopify.com') return true;
-    if (hostname.endsWith('.myshopify.com') &&
-        (pathname.startsWith('/checkouts') || pathname.startsWith('/checkout')))
-      return true;
-    return pathname.includes('/checkouts/');
-  } catch {
-    return false;
-  }
-};
-
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  const url = changeInfo.url || tab.url;
-  if (url && isShopifyCheckout(url)) {
-    chrome.action.openPopup({ tabId });
-  }
-});
-
-chrome.tabs.onActivated.addListener(async ({ tabId }) => {
-  try {
-    const tab = await chrome.tabs.get(tabId);
-    if (tab.url && isShopifyCheckout(tab.url)) {
-      chrome.action.openPopup({ tabId });
-    }
-  } catch (e) {
-    // Ignore errors
-  }
-});
-
 
